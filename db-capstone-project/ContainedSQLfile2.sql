faa144c63407e0e9502c89f9f41090f33fa55356
IngDanielGonzalez <117584552+IngDanielGonzalez@users.noreply.github.com>
2023-06-16 18:27:29 -0500

    Some modifications for static

--- db-capstone-project/ContainedSQLfile2.sql
+++ db-capstone-project/ContainedSQLfile2.sql
@@ -27,7 +27,7 @@ CREATE TABLE `bookings` (
   `booking_date` date NOT NULL,
   `table_number` int NOT NULL,
   PRIMARY KEY (`booking_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `bookings` (
 
 LOCK TABLES `bookings` WRITE;
 /*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+INSERT INTO `bookings` VALUES (1,'2023-09-09',1),(2,'2021-01-05',2),(3,'2023-08-01',3);
 /*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +56,7 @@ CREATE TABLE `costumer_details` (
   PRIMARY KEY (`customer_id`),
   KEY `fk_costumer_details_Bookings1_idx` (`booking_id_fk`),
   CONSTRAINT `fk_costumer_details_Bookings1` FOREIGN KEY (`booking_id_fk`) REFERENCES `bookings` (`booking_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,6 +65,7 @@ CREATE TABLE `costumer_details` (
 
 LOCK TABLES `costumer_details` WRITE;
 /*!40000 ALTER TABLE `costumer_details` DISABLE KEYS */;
+INSERT INTO `costumer_details` VALUES (1,'jose r','15479','joserthis.com',1),(2,'david w','65790','davidw@this.com',2),(3,'pedro i','24678','pedroi@this.com',3);
 /*!40000 ALTER TABLE `costumer_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,8 +83,9 @@ CREATE TABLE `menu` (
   `courses` varchar(45) NOT NULL,
   `drinks` varchar(45) NOT NULL,
   `desserts` varchar(45) NOT NULL,
+  `MenuName` varchar(255) NOT NULL,
   PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +94,35 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1,'Asian','sushi','bread','mojito','muffins','Land'),(2,'Mexican','Nachos','tacos','water','cake','River'),(3,'Europian','cheese','pizza','soda','cookies','Sea');
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menuitems`
+--
+
+DROP TABLE IF EXISTS `menuitems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menuitems` (
+  `MenuitemsID` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `CourseName` varchar(255) NOT NULL,
+  `StarterName` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `DesertName` varchar(255) DEFAULT NULL,
+  `menu_id` int DEFAULT NULL,
+  PRIMARY KEY (`MenuitemsID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menuitems`
+--
+
+LOCK TABLES `menuitems` WRITE;
+/*!40000 ALTER TABLE `menuitems` DISABLE KEYS */;
+INSERT INTO `menuitems` VALUES (1,'sushi','pizza','cake',1),(2,'pasta','rice','cokie',2),(3,'salad','rowfish','fruits',3);
+/*!40000 ALTER TABLE `menuitems` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -137,10 +168,11 @@ CREATE TABLE `orders` (
   `order_date` date NOT NULL,
   `total_cost` int NOT NULL,
   `customer_id_fk` int NOT NULL,
+  `menu_id` int DEFAULT NULL,
   PRIMARY KEY (`order_id`),
   KEY `customer_id_fk_idx` (`customer_id_fk`),
   CONSTRAINT `customer_id_fk` FOREIGN KEY (`customer_id_fk`) REFERENCES `costumer_details` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,6 +181,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,3,'2023-05-15',20,1,1),(2,4,'2023-06-13',100,2,2),(3,1,'2023-07-15',85,3,3);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,6 +213,20 @@ LOCK TABLES `orders_has_menu` WRITE;
 UNLOCK TABLES;
 
 --
+-- Temporary view structure for view `ordersview`
+--
+
+DROP TABLE IF EXISTS `ordersview`;
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ordersview` AS SELECT 
+ 1 AS `order_id`,
+ 1 AS `quantity`,
+ 1 AS `Cost`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `staff`
 --
 
@@ -202,6 +249,24 @@ LOCK TABLES `staff` WRITE;
 /*!40000 ALTER TABLE `staff` DISABLE KEYS */;
 /*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `ordersview`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `ordersview` AS select `orders`.`order_id` AS `order_id`,`orders`.`quantity` AS `quantity`,`orders`.`total_cost` AS `Cost` from `orders` where (`orders`.`quantity` > 2) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -212,4 +277,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-16 10:37:45
+-- Dump completed on 2023-06-16 18:24:01
